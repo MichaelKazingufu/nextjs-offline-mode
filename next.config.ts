aadfe39bef455c38c7c49558dfdc434d60e8ce49
unknown <bkmichael75@gmail.com>
2026-05-20 13:04:21 +0200

    pwa

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import nextPwa from "next-pwa";
 
 const withPWA = nextPwa({
   dest: "public",
+  register: true,
+  skipWaiting: true,
+
+  // IMPORTANT FIX
   disable: process.env.NODE_ENV === "development",
 });
 
